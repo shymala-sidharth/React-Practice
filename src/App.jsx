c29cc00dff95c9b5e4c3a58295d379b46fdd52cf
Shymala Sidharth <shymala.sidharth22@gmail.com>
2023-02-01 17:49:15 +1300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+import Header from './Header'
+
+import Footer from './Footer'
+import Employees from './Employees'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Employees />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
